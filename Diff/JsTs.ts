9841a05d12116470617f23c5eea06dbd62f95580
poojangit <poojang147@gmail.com>
2024-12-17 17:06:53 +0530

    [POOJA] Difference : Differences between javascript and typescript

---
+++ Diff/JsTs.ts
@@ -0,0 +1,19 @@
+//todo ------------Comparing ts with js-------------------
+
+//^ Javascript provide language primitives like string and number, but it doesn't check thatwe have consistenty assigned this. Typescript does.
+
+//^Javascript
+//! javascript is a lightweight, interpreted programming language used to create dynamic we content
+//! dynamically typed
+//! directly runs in the browser or nodejs
+//! does not support optional features like interface or enums
+//! Errors are caught only at the run time
+//! Used for small projects and prototypes
+
+//^Typescript
+//! Type script is a superset of javascript that adds static typing and additional features.
+//! Statically typed.
+//! needs to be compiled into javascript before running
+//! supports interface, enums, generics, and other fetatures.
+//! provides enchanced intellisense, better autocompletion and error reporting
+//! used for large-scale applications requiring maintainability
