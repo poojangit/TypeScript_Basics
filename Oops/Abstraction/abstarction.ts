9cbf33df1998610837558e7a3c46295e5952a5e7
poojangit <poojang147@gmail.com>
2024-12-18 18:08:13 +0530

    [POOJA] Abtraction : Overview of Abstraction and implementation part will be in a seperate folder of Abstract and Interface

---
+++ Oops/Abstraction/abstarction.ts
@@ -0,0 +1,7 @@
+//todo ---------Abstraction---------------------
+
+//^ Abstraction focuses on hiding the  implementaion details and exposing only the essensial features
+//^ It helps in managing complexity by seperating what an object does(interface) from how it does it(implemenation)
+//^ Typescript achives abstarction using
+//& Abstract class
+//& Interfaces
