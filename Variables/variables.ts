3eddc5e841cc7113941f2f51344aa5c8ff121021
poojangit <poojang147@gmail.com>
2024-12-18 12:00:08 +0530

    [POOJA] variables : Type annotations on variables , commented the code

--- Variables/variables.ts
+++ Variables/variables.ts
@@ -49,44 +49,44 @@
 
 //~ Example 4 - Array
 
-let arr1 : number[] = [1,2,3]
-console.log(arr1);
-let arr2 : Array<number> = [1,2,3]
-console.log(arr2)
-console.log(arr1[2]);
+// let arr1 : number[] = [1,2,3]
+// console.log(arr1);
+// let arr2 : Array<number> = [1,2,3]
+// console.log(arr2)
+// console.log(arr1[2]);
 
 //~ Exaple 5 - Tuple - for fixed values but different types
-let tuple1 : [number, string] 
-tuple1 = [10, "Pooja"]
+// let tuple1 : [number, string] 
+// tuple1 = [10, "Pooja"]
 // tuple1 = ["Hello", 100]
-console.log(tuple1);
-console.log(tuple1[1].substring(1));
+// console.log(tuple1);
+// console.log(tuple1[1].substring(1));
 
 //~Example 6 - Enum - giving more friendly names to sets of values
 
-enum Color {Blue=9, Green, Red}
-let c : Color = Color.Blue
-console.log(c);
+// enum Color {Blue=9, Green, Red}
+// let c : Color = Color.Blue
+// console.log(c);
 
-let colorName : string = Color[9]
-console.log(colorName);
+// let colorName : string = Color[9]
+// console.log(colorName);
 
 //~ Example 7 - Unknown -when discribing the variable without knowing the type of values it may have to store
 
-let notSure: unknown = 4
-notSure = "pooja"
-console.log(notSure);
+// let notSure: unknown = 4
+// notSure = "pooja"
+// console.log(notSure);
 
 //~ Example 8 - any - when not knowing the type of the value, can opt out of type checking using any
 // None of the following lines of code will throw compiler errors.
 // Using `any` disables all further type checking, and it is assumed
 // you know the environment better than TypeScript.
-let obj : any = {x : 0}
+// let obj : any = {x : 0}
 // obj.foo()
 // obj()
 // obj.bar = 100
-obj = "Hello"
-console.log(obj);
+// obj = "Hello"
+// console.log(obj);
 
 // let anyValue : any 
 // console.log(anyValue);
